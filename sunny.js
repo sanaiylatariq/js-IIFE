85d1206eef8816ab517116664d6fc3fbede4a15d
sanaiylatariq <sanaiylatariq517@gmail.com>
2024-07-10 21:10:23 +0500

    Add files via upload

---
+++ sunny.js
@@ -0,0 +1,73 @@
+// // immediately invoked function expression
+// // IIFE
+
+// // KISI B FUNCT KO EXECUTE KRAANY K LYE AP USLK BD SIMPLY BRACES LGAO R POORE FUNCTION LO B BRACES M DALO TO B WO EXECUTE HOJAEGA
+
+// (function one(){
+//      console.log(`hellllo`);
+//  })();
+
+
+
+
+
+
+//  // use semicolon to separate two iife otherwise errors will occur
+
+
+
+
+//   (  function one(){
+//         console.log(`hellllo`);
+//     }
+// )();
+
+// (
+//     ()=>{
+//         console.log(`hello world this is sanaiyla`);
+//     }
+// )()
+
+(
+//     function one(){
+//         console.log(`hello guys`);
+//     }
+// )();
+// (
+//      sunny = function(){
+//         console.log(`helllo bci`);
+//     }
+// )();
+// (
+//     two = ()=>{
+//         console.log(`helllo third vway`);
+//     }
+// )();
+// // 3 ways to declare function explained
+(
+//     (name)=>{
+// console.log(`hello her name is ${name}`);
+//     }
+// )(`sanaiyla`);
+// (
+//     (num1,num2)=> {return( `num1*num2`)}
+// )(3,3);
+// (
+//     (num1,num2)=>{
+// return num1*num2
+//     }
+// )(3,3)
+// ((num1, num2) => {
+//     return num1 * num2;
+// })(3, 3);
+
+// (
+//     ()=>{
+//         console.log(`hello zleel insan`)
+//     }
+// )()
+(
+    ()=>{
+        console.log(`hello zleel insan`);
+    }
+)())
